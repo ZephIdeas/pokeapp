8355bd10e0f80e2be3d7fbbe409560c5305cd168
ZephIdeas <hottekietquentin@orange.fr>
2019-03-29 13:58:33 +0100

    Pattern de base de l'app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main()=>runApp(MaterialApp(
 
@@ -7,13 +8,42 @@ void main()=>runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
 ));
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchData();
+  }
+
+  fetchData() async{}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Poke App"),
         backgroundColor: Colors.cyan,
+      ),
+
+      body: Center(
+        child: Text("Hello from Pokemon App"),
+      ) ,
+
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.cyan,
+        child: Icon(Icons.refresh),
+
       ),
     );
   }
