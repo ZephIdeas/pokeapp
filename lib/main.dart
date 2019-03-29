a3ccf767f1af0de1a2568eb06b65122cf0420a59
ZephIdeas <hottekietquentin@orange.fr>
2019-03-29 13:27:32 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MaterialApp(
+
+    title: "Poke App",
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+));
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Poke App"),
+        backgroundColor: Colors.cyan,
+      ),
+    );
+  }
+}
+
